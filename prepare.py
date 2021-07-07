f916b82591957cbf3cb343dae5a92bda51f5d482
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-07-07 21:57:13 +0200

    prepare fixes

--- prepare.py
+++ prepare.py
@@ -38,17 +38,25 @@ def prepare():
 		'bounds':   'bounds',
 	}
 
-	data = {}
+	data = {
+		'img':      '',
+		'pbf':      '',
+		'polygons': '',
+		'hgt':      '',
+		'temp':     '',
+		'sea':      '',
+		'bounds':   '',
+	}
 
 
 	#TODO kontroly validity vstupu
-	data['img'] = input('The name of the output folder with garmin maps (default maps): ')
-	data['pbf'] = input('The name of the folder for map data download (default pbf): ')
-	data['polygons'] = input('The name of the folder for polygons (default polygons): ')
-	data['hgt'] = input('The name of the folder for height data (default hgt): ')
-	data['temp'] = input('The name of the folder for temporary data (default temp): ')
-	data['sea'] = input('The name of the folder for sea data (default sea): ')
-	data['bounds'] = input('The name of the folder for bounds data (default bounds): ')
+	# data['img'] = input('The name of the output folder with garmin maps (default maps): ')
+	# data['pbf'] = input('The name of the folder for map data download (default pbf): ')
+	# data['polygons'] = input('The name of the folder for polygons (default polygons): ')
+	# data['hgt'] = input('The name of the folder for height data (default hgt): ')
+	# data['temp'] = input('The name of the folder for temporary data (default temp): ')
+	# data['sea'] = input('The name of the folder for sea data (default sea): ')
+	# data['bounds'] = input('The name of the folder for bounds data (default bounds): ')
 
 	for item in data:
 		if data[item] == '':
@@ -64,13 +72,13 @@ def prepare():
 	# TODO phyghtmap
 
 
-	if not os.path.exists('./sea.zip'):
+	if not os.path.exists(data['sea']):
 		d.download('http://osm.thkukuk.de/data/sea-latest.zip', './sea.zip')
 		with zipfile.ZipFile('./sea.zip', 'r') as zipRef:
 			zipRef.extractall(data['sea'])
 		#os.remove('./sea.zip')
 
-	if not os.path.exists('./bounds.zip'):
+	if not os.path.exists(data['bounds']):
 		d.download('http://osm.thkukuk.de/data/bounds-latest.zip', './bounds.zip')
 		with zipfile.ZipFile('./bounds.zip', 'r') as zipRef:
 			zipRef.extractall(data['bounds'])
