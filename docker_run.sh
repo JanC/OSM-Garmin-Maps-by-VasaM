89aefb390a75d84d3a174940ff79dcc16b122e17
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-07-10 16:54:31 +0200

    pwd for docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-mkdir -p /tmp/vasam/{maps,polygons,userAreas,pbf}
+mkdir -p ${PWD}/{maps,polygons,userAreas,pbf}
 
 area=$1
 
 docker run --rm -it \
-	-v /tmp/vasam/maps:/vasam/maps/ \
-	-v /tmp/vasam/polygons:/vasam/polygons/ \
-    -v /tmp/vasam/userAreas:/vasam/userAreas/ \
-	-v /tmp/vasam/pbf:/vasam/pbf/ \
+	-v ${PWD}/maps:/vasam/maps/ \
+	-v ${PWD}/polygons:/vasam/polygons/ \
+    -v ${PWD}/userAreas:/vasam/userAreas/ \
+	-v ${PWD}/pbf:/vasam/pbf/ \
 	-e area=${area} textwil/vasam
 
 
