d6692a39865120514c47afe29b5a8bd44254710b
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-07-10 20:38:22 +0200

    renamed docker image

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker build --tag textwil/vasam .
+docker build --tag textwil/vasam_garmin .
--- docker_run.sh
+++ docker_run.sh
@@ -1,24 +1,17 @@
 #! /bin/bash
 
-workdir=${PWD}
-workdir="/tmp/vasam"
-mkdir -p ${workdir}/{maps,polygons,userAreas,pbf}
 
 area=$1
+workdir=${2:-"output"}
+encoding=${3:-"unicode"}
+
+mkdir -p ${workdir}/{maps,polygons,userAreas,pbf}
 
 docker run --rm -it \
 	-v ${workdir}/maps:/vasam/maps/ \
 	-v ${workdir}/polygons:/vasam/polygons/ \
     -v ${workdir}/userAreas:/vasam/userAreas/ \
 	-v ${workdir}/pbf:/vasam/pbf/ \
-	-e area=${area} textwil/vasam
-
-
-# debug 
-# docker run --rm -it \
-# 	-v /tmp/vasam/maps:/vasam/maps/ \
-# 	-v /tmp/vasam/polygons:/vasam/polygons/ \
-#     -v /tmp/vasam/userAreas:/vasam/userAreas/ \
-# 	-v /tmp/vasam/pbf:/vasam/pbf/ \
-# 	-e area=${area} textwil/vasam /bin/bash
-
+	-e area=${area} \
+	-e encoding=${encoding} \
+	textwil/vasam_garmin
